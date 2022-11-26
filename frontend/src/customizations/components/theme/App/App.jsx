5ecab2bed8f3b475c34731f08bcfa49fa55813b3
Cihan Andac <cihanandac@gmail.com>
2022-11-26 21:10:14 +0300

    fixed document query rendering problem

--- frontend/src/customizations/components/theme/App/App.jsx
+++ frontend/src/customizations/components/theme/App/App.jsx
@@ -232,7 +232,7 @@ class App extends Component {
   }
 }
 
-{document.getElementById('main').style.width = `calc(100vw - ${window.innerWidth - document.body.clientWidth}px)`}
+{if(typeof window !== 'undefined'){document.getElementById('main').style.width = `calc(100vw - ${window.innerWidth - document.body.clientWidth}px)`}}
   
 
 export const __test__ = connect(
